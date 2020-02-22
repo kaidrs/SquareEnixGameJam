e94625d17f1698fb87fa1fc7b8a09ff4f7e5333e
GrisWoldDiablo <griswoldd@hotmail.com>
2020-02-21 21:34:12 -0500

    Rename GM to NetworkManager

--- SquareEnixGameJam/Assets/Scripts/PlayerName.cs
+++ SquareEnixGameJam/Assets/Scripts/PlayerName.cs
@@ -6,14 +6,13 @@ using System.Collections.Generic;
 public class PlayerName : MonoBehaviour
 {
     [SerializeField] private Text playerNames;
-    List<Photon.Realtime.Player> photonPlayers;
     // Start is called before the first frame update
     void Start()
     {
-        
-        foreach (var player in photonPlayers)
+        playerNames.text += $"\nYou: {NetworkManager.Instance.myPlayer}";
+        foreach (var player in NetworkManager.Instance.photonPlayers)
         {
-            playerNames.text += "\n" + photonPlayers;
+            playerNames.text += $"\n{player}";
         }
     }
 
