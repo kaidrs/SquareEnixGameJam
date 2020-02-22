287db1ae32b2eb2dfc8ff074255a594bd57392f1
Alex Flores <SorAxel360@hotmail.com>
2020-02-21 23:56:45 -0500

    Added Card Objets
    + Loot card Objets

--- SquareEnixGameJam/Assets/Scripts/Cards/Card.cs
+++ SquareEnixGameJam/Assets/Scripts/Cards/Card.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[SerializeField]
-public class Card : MonoBehaviour
+public class Card : ScriptableObject
 {
-    private string cardName;
-    private int cardNumber;
+    [Header("Card Properties")]
+    [SerializeField] private string cardName;
+    [SerializeField] private int cardNumber;
 
-    protected Card(string name, int number)
-    {
-        this.cardName = name;
-        this.cardNumber = number;
-    }
+    [SerializeField] private Sprite cardSprite;
 }
