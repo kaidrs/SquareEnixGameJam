c8fc9b1b03b27a22e788fcadc4ae67ff766c101e
Jeff <droodic.gamedev@gmail.com>
2020-02-22 11:15:44 -0500

    Auto stash before merge of "master" and "origin/master"

--- SquareEnixGameJam/Assets/Scripts/Managers/InventoryManager.cs
+++ SquareEnixGameJam/Assets/Scripts/Managers/InventoryManager.cs
@@ -5,29 +5,29 @@ using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField] GameObject heroSprite;
+    private Hero hero; //change later
+
+   // [SerializeField] GameObject heroSprite;
     [SerializeField] Text heroNameText;
     [SerializeField] Text heroClassText;
     [SerializeField] Text heroAttackText;
     [SerializeField] Text heroDefenseText;
     [SerializeField] Text heroLuckText;
-    [SerializeField] Text heroSpellText;
+   // [SerializeField] Text heroSpellText;
 
-    [SerializeField] Image heroWeaponImage;
-    [SerializeField] Image heroDefenseImage;
-    [SerializeField] Image heroLuckImage;
-    [SerializeField] Image heroSpellImage;
+  //  [SerializeField] Image heroWeaponImage;
+   // [SerializeField] Image heroDefenseImage;
+   // [SerializeField] Image heroLuckImage;
+    //[SerializeField] Image heroSpellImage;
 
-    //Get Hero photon shananagin later
-    Hero hero;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        hero = GetComponent<Hero>();
-
-
+        hero = HeroManager.playerChoice[0];
+        InitInventoryUI();
+        
     }
 
 
@@ -38,7 +38,7 @@ public class InventoryManager : MonoBehaviour
         heroAttackText.text = hero.AttackPoint.ToString();
         heroDefenseText.text = hero.DefencePoint.ToString();
         heroLuckText.text = hero.Luck.ToString();
-        //heroSpellText.text = SpellCard.name
+        //spellcard name?
 
     }
 
