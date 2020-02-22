b1975a796a1b96dfccfd97f4a88d3b800658d132
Angeline <siv.angeline@ymail.com>
2020-02-21 21:36:59 -0500

    Created TileManager

---
+++ SquareEnixGameJam/Assets/Scripts/TileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    #region TileManager Singleton
+    private static TileManager _instance = null;
+    public static TileManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<TileManager>();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
