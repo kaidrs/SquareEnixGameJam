a76e3d5bdea2b00e1e27c9444da11a1d95b4d54e
Angeline <siv.angeline@ymail.com>
2020-02-21 23:29:43 -0500

    Updated TileManager

--- SquareEnixGameJam/Assets/Scripts/TileManager.cs
+++ SquareEnixGameJam/Assets/Scripts/TileManager.cs
@@ -1,6 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum TileType
+{
+    None, Loot, Spell, Monster
+}
+
+public enum ZoneType
+{
+    StarterZone, MiddleZone, FinalZone
+}
+
+[Serializable]
+public class Zone
+{
+    public string name;
+    [SerializeField] private ZoneType type;
+    [SerializeField] private List<TileType> numberOfTiles;
+    private float zoneMultiplier = 1.0f;
+
+    public float GetZoneMultiplier()
+    {
+        switch(type)
+        {
+            case ZoneType.StarterZone:
+                return zoneMultiplier = 1.0f;
+            case ZoneType.MiddleZone:
+                return zoneMultiplier = 1.5f;
+            case ZoneType.FinalZone:
+                return zoneMultiplier = 2.0f;
+            default:
+                return 0;
+        }
+    }
+}
 
 public class TileManager : MonoBehaviour
 {
@@ -19,15 +54,28 @@ public class TileManager : MonoBehaviour
     }
     #endregion
 
-    // Start is called before the first frame update
+    //Need player class here
+    [SerializeField] private List<Zone> numberOfZones;
+    private int playerTileLocation;
+
     void Start()
     {
-        
+        this.playerTileLocation = 0;
+
+        //find Object of Type Player
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Moves the player tile location according to the dice value
+    /// </summary>
+    /// <param name="diceValue"></param>
+    public void SetPlayerTileLocation(int diceValue)
     {
-        
+        this.playerTileLocation += diceValue;
+    }
+
+    public void PromptCardTile()
+    {
+        //Scene change which depends on the event
     }
 }
