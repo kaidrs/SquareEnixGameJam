dbbbdca336efe3b349d8249bd4090ce525c3a280
Minh <anhtm.mtl@gmail.com>
2020-02-22 17:08:01 -0500

    whatever

--- SquareEnixGameJam/Assets/Scripts/Dice/DiceTossScripts/DiceCheckZoneScript.cs
+++ SquareEnixGameJam/Assets/Scripts/Dice/DiceTossScripts/DiceCheckZoneScript.cs
@@ -11,39 +11,39 @@ public class DiceCheckZoneScript : MonoBehaviour {
 		diceVelocity = DiceScript.diceVelocity;
 	}
 
-    private void OnCollisionStay(Collision collision)
-    {
-        
-    }
-
     void OnTriggerStay(Collider col)
 	{
         if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f)
         {
-            switch (col.gameObject.name)
+            if (col.gameObject.tag == "DiceZone")
             {
-                case "Side1":
-                    DiceManager.Instance.CurrentValue = 6;
-                    break;
-                case "Side2":
-                    DiceManager.Instance.CurrentValue = 5;
-                    break;
-                case "Side3":
-                    DiceManager.Instance.CurrentValue = 4;
-                    break;
-                case "Side4":
-                    DiceManager.Instance.CurrentValue = 3;
-                    break;
-                case "Side5":
-                    DiceManager.Instance.CurrentValue = 2;
-                    break;
-                case "Side6":
-                    DiceManager.Instance.CurrentValue = 1;
-                    break;
+                Debug.Log("col.gameObject.name: " + col.gameObject.name);
+                switch (col.gameObject.name)
+                {
+                    case "Side1":
+                        DiceManager.Instance.CurrentValue = 6;
+                        break;
+                    case "Side2":
+                        DiceManager.Instance.CurrentValue = 5;
+                        break;
+                    case "Side3":
+                        DiceManager.Instance.CurrentValue = 4;
+                        break;
+                    case "Side4":
+                        DiceManager.Instance.CurrentValue = 3;
+                        break;
+                    case "Side5":
+                        DiceManager.Instance.CurrentValue = 2;
+                        break;
+                    case "Side6":
+                        DiceManager.Instance.CurrentValue = 1;
+                        break;
+                }
+                Debug.Log("Upper side is now: " + DiceManager.Instance.CurrentValue);
             }
+
 
         }
 
-        Debug.Log("Upper side is now: " + DiceManager.Instance.CurrentValue);
     }
 }
