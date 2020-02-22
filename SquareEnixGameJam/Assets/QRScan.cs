f3aa81619fb768976427573110c16b4876bfc18a
Jeff <droodic.gamedev@gmail.com>
2020-02-22 15:04:43 -0500

    qr code big push

--- SquareEnixGameJam/Assets/QRScan.cs
+++ SquareEnixGameJam/Assets/QRScan.cs
@@ -3,42 +3,58 @@ using System.Collections.Generic;
 using ZXing;
 using ZXing.QrCode;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class QRScan : MonoBehaviour
 {
 
-
-private WebCamTexture camTexture;
-private Rect screenRect;
-void Start()
-{
-    screenRect = new Rect(0, 0, Screen.width, Screen.height);
-    camTexture = new WebCamTexture();
-    camTexture.requestedHeight = Screen.height;
-    camTexture.requestedWidth = Screen.width;
-    if (camTexture != null)
+    bool runningCam;
+    public Text scanText;
+    public WebCamTexture camTexture;
+    private Rect screenRect;
+    void Start()
     {
-        camTexture.Play();
-    }
-}
-
-void OnGUI()
-{
-    // drawing the camera on screen
-    GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
-    // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
-    try
-    {
-        IBarcodeReader barcodeReader = new BarcodeReader();
-        // decode the current frame
-        var result = barcodeReader.Decode(camTexture.GetPixels32(),
-          camTexture.width, camTexture.height);
-        if (result != null)
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTexture = new WebCamTexture();
+        //stopTexture = new WebCamTexture();
+        camTexture.requestedHeight = Screen.height;
+        camTexture.requestedWidth = Screen.width;
+        if (camTexture != null)
         {
-            Debug.Log("DECODED TEXT FROM QR: " +result.Text);
+            camTexture.Play();
+            runningCam = true;
         }
     }
-    catch (Exception ex) { Debug.LogWarning(ex.Message); }
-}
+
+    void OnGUI()
+    {
+        if (runningCam && camTexture!= null)
+        {
+            // drawing the camera on screen
+            GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+            // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+            try
+            {
+                IBarcodeReader barcodeReader = new BarcodeReader();
+                // decode the current frame
+                var result = barcodeReader.Decode(camTexture.GetPixels32(),
+                  camTexture.width, camTexture.height);
+                if (result != null)
+                {
+                    Debug.Log("DECODED TEXT FROM QR: " + result.Text);
+                    scanText.text = result.Text;
+
+                    //camTexture.Stop();
+                    runningCam = false;
+                    FindObjectOfType<QRManager>().killIt();
+
+
+                }
+
+            }
+            catch (Exception ex) { Debug.LogWarning(ex.Message); }
+        }
+        
+    }
 }
---
+++ SquareEnixGameJam/Assets/Scripts/QRManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QRManager : MonoBehaviour
+{
+    [SerializeField] QRScan qrObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartQR", 3f);
+        Invoke("killIt", 7f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartQR()
+    {
+        Instantiate(qrObject);
+    }
+
+    public void killIt()
+    {
+        qrObject = FindObjectOfType<QRScan>();
+        qrObject.camTexture.Stop();
+        Destroy(qrObject);
+        Debug.Log("killed");
+    }
+}
