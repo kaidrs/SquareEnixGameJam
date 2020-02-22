784a3a6fcaea5bae7edd2983125b3e57a2f3ddba
Alex Flores <SorAxel360@hotmail.com>
2020-02-21 22:37:12 -0500

    Add Card Class
    + MonsterCard:Card
    + SpellCard:Card
    + EventCard:Card
    + LootCard:Card

---
+++ SquareEnixGameJam/Assets/Scripts/Cards/LootCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootCard : Card
+{
+	private int attackStat;
+	private int defenceStat;
+
+	public LootCard(string name, int number, int attack, int defence):base(name, number)
+	{
+		this.attackStat = attack;
+		this.defenceStat = defence;
+	}
+
+	public void AddStatToPlayer(/*Hero hero*/)
+	{
+		//hero.attack += this.attackStat;
+		//hero.defence += this.defenceStat;
+	}
+}
---
+++ SquareEnixGameJam/Assets/Scripts/Cards/SpellCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCard : Card
+{
+	public SpellCard(string name, int number):base(name, number) 
+	{
+		
+	}
+
+	public void CastSpell()
+	{
+
+	}
+}
