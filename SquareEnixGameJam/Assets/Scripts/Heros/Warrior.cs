8276d9d4a2d2c6f02e5c19bc9c3243340906300a
Robert <roobtran@gmail.com>
2020-02-21 22:33:26 -0500

    +Added hero and child class

---
+++ SquareEnixGameJam/Assets/Scripts/Heros/Warrior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : Hero
+{
+    public Warrior()
+    {
+        this.HealthPoint = 1.0f;
+        this.AttackPoint = 5;
+        this.DefencePoint = 2;
+        this.Luck = 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
