020d6b9d89352d7d24a53ea77bba646727898d24
Jeff <droodic.gamedev@gmail.com>
2020-02-21 23:22:33 -0500

    dicemanager begin

--- SquareEnixGameJam/Assets/Scripts/Cards/MonsterCard.cs
+++ SquareEnixGameJam/Assets/Scripts/Cards/MonsterCard.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class MonsterCard : Card
 {
-	public MonsterCard(string name, int number):base(name, number)
+    float health = 1;
+    int damage;
+    int defense;
+
+	public MonsterCard(string name, int number, int damage, int defense):base(name, number)
 	{
-		
+        this.damage = damage;
+        this.defense = defense;
 	}
+
+    public void TakeDamage(Hero hero)
+    {
+        float damageMultiplier = (100 / (100 + defense)); //Clamp to 0-100 percentage reduction
+        this.health -= (hero.AttackPoint * damageMultiplier);
+    }
+
 }
