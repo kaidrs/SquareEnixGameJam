339ce4f3bbc4147bfd50d7ea76d0b32761d74c7b
Minh <anhtm.mtl@gmail.com>
2020-02-22 16:23:28 -0500

    Add dice

---
+++ SquareEnixGameJam/Assets/Scripts/Dice/DiceTossScripts/DiceCheckZoneScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceCheckZoneScript : MonoBehaviour {
+
+	Vector3 diceVelocity;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		diceVelocity = DiceScript.diceVelocity;
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    void OnTriggerStay(Collider col)
+	{
+        if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f)
+        {
+            switch (col.gameObject.name)
+            {
+                case "Side1":
+                    DiceManager.Instance.CurrentValue = 6;
+                    break;
+                case "Side2":
+                    DiceManager.Instance.CurrentValue = 5;
+                    break;
+                case "Side3":
+                    DiceManager.Instance.CurrentValue = 4;
+                    break;
+                case "Side4":
+                    DiceManager.Instance.CurrentValue = 3;
+                    break;
+                case "Side5":
+                    DiceManager.Instance.CurrentValue = 2;
+                    break;
+                case "Side6":
+                    DiceManager.Instance.CurrentValue = 1;
+                    break;
+            }
+
+        }
+
+        Debug.Log("Upper side is now: " + DiceManager.Instance.CurrentValue);
+    }
+}
---
+++ SquareEnixGameJam/Assets/Scripts/Dice/DiceTossScripts/DiceScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceScript : MonoBehaviour {
+
+	static Rigidbody rb;
+	public static Vector3 diceVelocity;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+    public void RollDice()
+    {
+        diceVelocity = rb.velocity;
+        DiceNumberTextScript.diceNumber = 0;
+        float dirX = Random.Range(0, 500);
+        float dirY = Random.Range(0, 500);
+        float dirZ = Random.Range(0, 500);
+        transform.position = new Vector3(0, 2, 0);
+        transform.rotation = Quaternion.identity;
+        rb.AddForce(transform.up * 500);
+        rb.AddTorque(dirX, dirY, dirZ);
+    }
+}
